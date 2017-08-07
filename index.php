58edba9c06bd52d5a4b717951d56cd2837924fd9
steven hebert <shebert2@cnm.edu>
2017-08-07 02:15:20 -0600

    committing js practice files

---
+++ index.php
@@ -0,0 +1,43 @@
+/**
+* create a GitHub repository/deployment put in some filler text
+* write ≥ 3 functions to manipulate the filler text/images:
+* 1) change colors
+* 2) change fonts
+* 3) find/replace
+* use an <input> tag at some point to ask for user input
+* put your CSS, JavaScript, and image files in separate files - NO INLINE
+* connect those functions to the event(s) of your choice
+**/
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+				integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+				  integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+				  crossorigin="anonymous"></script>
+
+		<!-- jQuery v3.0 -->
+		<script>src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"</script>
+
+		<script type="text/javascript" src="script.js"></script>
+
+		<title>JS Assignment</title>
+
+	</head>
+
+	<body>
+	</body>
+</html>
