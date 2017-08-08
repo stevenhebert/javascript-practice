18a0e38e0ef5b5d14bec37fbd0b27f56c51e9d18
steven hebert <shebert2@cnm.edu>
2017-08-08 15:54:47 -0600

    added index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>JS Practice</title>
+	</head>
+	<body>
+		<link href="https://bootcamp-coders.cnm.edu/~shebert2/javascript-practice/registration.php">
+		<link href="https://bootcamp-coders.cnm.edu/~shebert2/javascript-practice/other.php">
+		<img src="images/javaNotJavaS.jpeg">
+	</body>
+</html>
+
+
