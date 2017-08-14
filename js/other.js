c84ae77ca30d325ec5fc1b39e560a59b0b5a8a7e
shebert2 <shebert2@cnm.edu>
2017-08-14 03:59:18 -0600

    ajax practice

--- js/other.js
+++ js/other.js
@@ -34,60 +34,87 @@ function unchangeImage() {
 }
 
 /**
-// data that lives in an object
-var myCat = {
+ // data that lives in an object
+ var myCat = {
 	"name": "meowsAlot",
 	"species": "cat",
 	"favFood": "tuna"
 }
 
-// myCat.name
-// myCat.species
-// myCat.favFood
+ // myCat.name
+ // myCat.species
+ // myCat.favFood
 
 
-// data that lives in an array
-var myFavColors = ["blue", "green", "purple"];
+ // data that lives in an array
+ var myFavColors = ["blue", "green", "purple"];
 
-// myFavColors[1]
-// returns green
+ // myFavColors[1]
+ // returns green
 
 
-// array of objects
-var thePets = [
-	{
-		"name": "meowsAlot",
-		"species": "cat",
-		"favFood": "tuna"
-	},
-	{
-		"name": "meowsAlot",
-		"species": "cat",
-		"favFood": "tuna"
-	}
-]
+ // array of objects
+ var thePets = [
+ {
+	 "name": "meowsAlot",
+	 "species": "cat",
+	 "favFood": "tuna"
+ },
+ {
+	 "name": "meowsAlot",
+	 "species": "cat",
+	 "favFood": "tuna"
+ }
+ ]
 
-// thePets.favFood[1]
-// object.property
-// returns carrots
+ // thePets.favFood[1]
+ // object.property
+ // returns carrots
 
-**/
+ **/
+var pageCounter = 1;
 var animalContainer = document.getElementById("animal-info");
-var button = document.getElementsById("btn")
+var btn = document.getElementsById("btn");
 
 btn.addEventListener("click", function() {
 
 	var ourRequest = new XMLHttpRequest();
-	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
 	ourRequest.onload = function() {
 		var ourData = JSON.parse(ourRequest.responseText);
-renderHTML(ourData);
-
+		renderHTML(ourData);
 	};
-	ourRequest.send;
+	ourRequest.send();
+	pageCounter++;
 });
 
-function renderHTML(data){
-	var htmlstring = "this is a test"
-animalContainer.insertAdjacentHTML('beforeend', 'htmlstring')
+function renderHTML(data) {
+	var htmlString = "";
+
+	for(i = 0, i < data.length; i++) {
+		htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+
+		for(ii = 0, ii < data[i].food.likes.length; ii++) {
+			if(ii == 0) {
+				htmlString += data[i].food.likes.[ii];
+			} else {
+				htmlString += " and "
+				data[i].food.likes.[ii];
+			}
+		}
+
+		htmlString += ' and dislikes ';
+
+		for(ii = 0, ii < data[i].food.dislikes.length; ii++) {
+			if(ii == 0) {
+				htmlString += data[i].food.dislikes.[ii];
+			} else {
+				htmlString += " and "
+				data[i].food.dislikes.[ii];
+			}
+		}
+
+		htmlString += '.<p/>';
+	}
+	animalContainer.insertAdjacentHTML('beforeend', 'htmlString');
 }
