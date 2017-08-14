ca371628c6af8c8fe2ecc463061f18cb8d0f656f
shebert2 <shebert2@cnm.edu>
2017-08-14 03:24:49 -0600

    closure

--- js/other.js
+++ js/other.js
@@ -33,7 +33,61 @@ function unchangeImage() {
 	document.getElementById('imageChange').src = "images/bliss.jpg";
 }
 
-
+/**
+// data that lives in an object
 var myCat = {
-"name":
+	"name": "meowsAlot",
+	"species": "cat",
+	"favFood": "tuna"
+}
+
+// myCat.name
+// myCat.species
+// myCat.favFood
+
+
+// data that lives in an array
+var myFavColors = ["blue", "green", "purple"];
+
+// myFavColors[1]
+// returns green
+
+
+// array of objects
+var thePets = [
+	{
+		"name": "meowsAlot",
+		"species": "cat",
+		"favFood": "tuna"
+	},
+	{
+		"name": "meowsAlot",
+		"species": "cat",
+		"favFood": "tuna"
+	}
+]
+
+// thePets.favFood[1]
+// object.property
+// returns carrots
+
+**/
+var animalContainer = document.getElementById("animal-info");
+var button = document.getElementsById("btn")
+
+btn.addEventListener("click", function() {
+
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+	ourRequest.onload = function() {
+		var ourData = JSON.parse(ourRequest.responseText);
+renderHTML(ourData);
+
+	};
+	ourRequest.send;
+});
+
+function renderHTML(data){
+	var htmlstring = "this is a test"
+animalContainer.insertAdjacentHTML('beforeend', 'htmlstring')
 }
